16a599a666ded045dea8da893815bb663255f66f
JoaoGabrielOliveira <joconceicao@bionexo.com>
2020-09-19 23:13:39 -0300

    [dbo]refatory insert

--- db_operations/config/db/db_operation.php
+++ db_operations/config/db/db_operation.php
@@ -25,31 +25,6 @@
             }
         }
 
-        public static function select(PDO $connection,$table_name, $collumns = '*', $condition = '')
-        {
-            try
-            {
-                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
-
-                $condition = self::creating_condition($condition);
-
-                $statement = $connection->prepare("SELECT $collumns FROM $table_name $condition");
-                $statement->execute();
-                $result = $statement->fetchAll(PDO::FETCH_CLASS);
-
-                $connection = null;
-
-                return $result;
-            }
-
-            catch(PDOException $e)
-            {
-                return "Error: " . $e->getMessage();
-                die();
-            }
-        }
-
         public static function insert(PDO $connection,array $values)
         {
             $insert_results = 0;
@@ -158,44 +133,6 @@
         }
 
         /* HELPERS */
-        private static function processing_data(array $data)
-        {
-            $processed_data = [];
-
-            foreach($data as $index => $row)
-            {
-                $insert_data = [];                    
-                foreach($row as $key=>$value)
-                {
-                    $index_value = ':' . $index . $key;
-                    $insert_data[$index_value] = $value;
-                }
-
-                array_push($processed_data,$insert_data);
-            }
-            return $processed_data;
-        }
-
-        private static function processing_multiple_data(array $data)
-        {
-            $processed_data = [];
-
-            foreach($data as $index => $value)
-            {
-                $index_value = ':' . $index;
-                $processed_data[$index_value] = $value;
-            }
-
-            return $processed_data;
-        }
-
-        private static function is_mutiple_values(array $data)
-        {
-            $keys = array_keys($data);
-            $is_int = is_int($keys[0]);
-
-            return $is_int;
-        }
 
         private static function creating_condition($condition)
         {
@@ -249,35 +186,6 @@
             return( implode(',',$result) );
         }
 
-        private static function convert_data_to_insert_sql($table_name, $collumns,$rows)
-        {
-            $sql_values = self::convert_values_to_sql($rows);
-
-            return "INSERT INTO $table_name ($collumns) VALUES " . implode(',',$sql_values);
-        }
-
-
-        private static function convert_values_to_sql(array $data)
-        {
-            $result = [];
-
-            foreach($data as $key)
-            {
-                $is_array = is_array($key);
-                $keys = ($is_array) ? array_keys($key) : array_keys($data);
-
-                $string_keys = implode("," , $keys);
-
-                $string_keys = "(" . $string_keys .")";
-
-                array_push($result,$string_keys);
-
-                if (!$is_array) break;
-            }
-
-            return $result;
-        }
-
         private static function merge_data(array $data)
         {
             $marged_data = [];
@@ -290,4 +198,7 @@
             return $marged_data;
         }
     }
+
+    include_once dirname(__FILE__) . '/CRUD/select.php';
+    include_once dirname(__FILE__) . '/CRUD/insert.php';
 ?>
