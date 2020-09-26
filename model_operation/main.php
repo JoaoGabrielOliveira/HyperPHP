78cfe47e23f44d83e281cd72b8bc5964c2c5fa1b
JoaoGabrielOliveira <joconceicao@bionexo.com>
2020-09-26 16:44:13 -0300

    [model]Creating functions

---
+++ model_operation/main.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once '/home/bionexo/Documents/projetos_paralelos/HyperPHP/db_operations/config/db/db_operation.php';
+
+?>
---
+++ model_operation/src/model.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hyper\Model;
+
+require_once dirname(__FILE__) . '/../main.php';
+
+class model_base
+{
+    protected $table;
+
+    public static function find(int $id)
+    {
+
+    }
+
+    public static function find_by($params)
+    {
+
+    }
+
+    public static function all()
+    {
+        
+    }
+
+    public static function where($condition)
+    {
+
+    }
+
+    public static function create($params)
+    {
+
+    }
+
+    public static function update()
+    {
+
+    }
+
+    public static function delete()
+    {
+
+    }
+
+    public function save()
+    {
+
+    }    
+
+}
+
+?>
